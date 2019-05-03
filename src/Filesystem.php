372c2d6aedaefb0d694c69c47c17928b32c6f7ec
Daniel Mecke <mecke@kigaroo.de>
2019-05-03 11:34:17 +0200

    removed unused methods

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -6,19 +6,14 @@ namespace KigaRoo;
 
 final class Filesystem
 {
-    /** 
-     * @var string 
+    /**
+     * @var string
      */
     private $basePath;
 
     public function __construct(string $basePath)
     {
         $this->basePath = $basePath;
-    }
-
-    public static function inDirectory(string $path): self
-    {
-        return new self($path);
     }
 
     public function path(string $filename): string
@@ -29,34 +24,6 @@ final class Filesystem
     public function has(string $filename): bool
     {
         return file_exists($this->path($filename));
-    }
-
-    /**
-     * Get all file names in this directory that have the same name
-     * as $fileName, but have a different file extension.
-     *
-     */
-    public function getNamesWithDifferentExtension(string $fileName): array
-    {
-        if (! file_exists($this->basePath)) {
-            return [];
-        }
-
-        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-
-        $baseName = substr($fileName, 0, strlen($fileName) - strlen($extension) - 1);
-
-        $allNames = scandir($this->basePath);
-
-        $namesWithDifferentExtension = array_filter($allNames, function ($existingName) use ($baseName, $extension) {
-            $existingExtension = pathinfo($existingName, PATHINFO_EXTENSION);
-
-            $existingBaseName = substr($existingName, 0, strlen($existingName) - strlen($existingExtension) - 1);
-
-            return $existingBaseName === $baseName && $existingExtension !== $extension;
-        });
-
-        return array_values($namesWithDifferentExtension);
     }
 
     public function read(string $filename): string
@@ -71,24 +38,5 @@ final class Filesystem
         }
 
         file_put_contents($this->path($filename), $contents);
-    }
-
-    public function delete(string $fileName): bool 
-    {
-        return unlink($this->path($fileName));
-    }
-
-    public function copy(string $filePath, string $fileName): void
-    {
-        if (! file_exists($this->basePath)) {
-            mkdir($this->basePath);
-        }
-
-        copy($filePath, $this->path($fileName));
-    }
-
-    public function fileEquals(string $filePath, string $fileName): bool 
-    {
-        return sha1_file($filePath) === sha1_file($this->path($fileName));
     }
 }
