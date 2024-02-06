f6f794ea98f9c63e9989e3be6ca8836c9072f618
Danu Bekas <mail@dbekas.de>
2024-02-06 13:29:29 +0100

    add StringOrNullWildcard

---
+++ src/Wildcard/StringOrNullWildcard.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KigaRoo\SnapshotTesting\Wildcard;
+
+use InvalidArgumentException;
+use Webmozart\Assert\Assert;
+
+final class StringOrNullWildcard implements Wildcard
+{
+    /** @var string */
+    private $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function atPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $mixed
+     */
+    public function match($mixed): bool
+    {
+        if (null === $mixed) {
+            return true;
+        }
+
+        try {
+            Assert::string($mixed);
+
+            return true;
+        } catch (InvalidArgumentException $exception) {
+            return false;
+        }
+    }
+}
