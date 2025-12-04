b330a0cd4dc7963dda0ad1e091e9a15a48999ae2
nwinzer <norman.winzer@bitladen.de>
2025-12-04 11:46:43 +0100

    add FloatWildcard (suitable for JSON only, like IntegerWildcard)

--- tests/unit/JsonTest.php
+++ tests/unit/JsonTest.php
@@ -9,6 +9,7 @@ use KigaRoo\SnapshotTesting\MatchesSnapshots;
 use KigaRoo\SnapshotTesting\Wildcard\BooleanWildcard;
 use KigaRoo\SnapshotTesting\Wildcard\DateTimeOrNullWildcard;
 use KigaRoo\SnapshotTesting\Wildcard\DateTimeWildcard;
+use KigaRoo\SnapshotTesting\Wildcard\FloatWildcard;
 use KigaRoo\SnapshotTesting\Wildcard\IntegerWildcard;
 use KigaRoo\SnapshotTesting\Wildcard\UuidWildcard;
 use KigaRoo\SnapshotTesting\Wildcard\Wildcard;
@@ -24,62 +25,68 @@ final class JsonTest extends TestCase
         $data = json_encode(
             [
                 'tests' =>
-             [
-                 'id' => 'b84c9b7f-1ebb-49b6-9d18-4305932b2dd1',
-                 'nested1' => [
-                     0 => [
-                         'nested2' => [
-                             0 => ['int' => 2],
-                             1 => ['int' => 3],
-                         ],
-                     ],
-                     1 => [
-                         'nested2' => [
-                             0 => ['int' => 4],
-                             1 => ['int' => 5],
-                         ],
-                     ],
-                 ],
-                 'multiple' => [
-                     0 => 'a',
-                     1 => 6665,
-                     2 => 'c',
-                 ],
-                 'integers' => [
-                     123,
-                     1234,
-                     12345,
-                     123456,
-                 ],
-                 'booleans' => [
-                     true,
-                     true,
-                     false,
-                 ],
-                 'objects' => [
-                     ['id' => 123],
-                     ['id' => 234],
-                     ['id' => 345],
-                 ],
-                 'arrays' => [
-                     [1,2,3],
-                     [3,4,5],
-                 ],
-                 'dateTimes' => [
-                     [
-                         '2019-07-10T12:50:20+00:00',
-                         '2019-02-28T13:50:24+02:00',
-                     ],
-                     [
-                         '02.02.2012 12:12',
-                         '01.01.2011 11:11',
-                     ],
-                 ],
-                 'dateTimeOrNull' => [
-                     '2019-07-10T12:50:20+00:00',
-                     null,
-                 ],
-             ],
+                    [
+                        'id' => 'b84c9b7f-1ebb-49b6-9d18-4305932b2dd1',
+                        'nested1' => [
+                            0 => [
+                                'nested2' => [
+                                    0 => ['int' => 2],
+                                    1 => ['int' => 3],
+                                ],
+                            ],
+                            1 => [
+                                'nested2' => [
+                                    0 => ['int' => 4],
+                                    1 => ['int' => 5],
+                                ],
+                            ],
+                        ],
+                        'multiple' => [
+                            0 => 'a',
+                            1 => 6665,
+                            2 => 'c',
+                        ],
+                        'integers' => [
+                            123,
+                            1234,
+                            12345,
+                            123456,
+                        ],
+                        'booleans' => [
+                            true,
+                            true,
+                            false,
+                        ],
+                        'float' => 3.2,
+                        'floats' => [
+                            2.2,
+                            1.4,
+                            3.1,
+                        ],
+                        'objects' => [
+                            ['id' => 123],
+                            ['id' => 234],
+                            ['id' => 345],
+                        ],
+                        'arrays' => [
+                            [1,2,3],
+                            [3,4,5],
+                        ],
+                        'dateTimes' => [
+                            [
+                                '2019-07-10T12:50:20+00:00',
+                                '2019-02-28T13:50:24+02:00',
+                            ],
+                            [
+                                '02.02.2012 12:12',
+                                '01.01.2011 11:11',
+                            ],
+                        ],
+                        'dateTimeOrNull' => [
+                            '2019-07-10T12:50:20+00:00',
+                            null,
+                        ],
+                    ],
             ]
         );
 
@@ -89,6 +96,8 @@ final class JsonTest extends TestCase
             new IntegerWildcard('tests.multiple[1]'),
             new IntegerWildcard('tests.integers[*]'),
             new BooleanWildcard('tests.booleans[*]'),
+            new FloatWildcard('tests.float'),
+            new FloatWildcard('tests.floats[*]'),
             new IntegerWildcard('tests.objects[*].id'),
             new IntegerWildcard('tests.arrays[*][2]'),
             new DateTimeWildcard('tests.dateTimes[0][*]'),
@@ -107,53 +116,53 @@ final class JsonTest extends TestCase
         $data = json_encode(
             [
                 'tests' =>
-             [
-                 'id' => 'b84c9b7f-1ebb-49b6-9d18-4305932b2dd1',
-                 'nested1' => [
-                     0 => [
-                         'nested2' => [
-                             0 => ['int' => 2],
-                             1 => ['int' => 3],
-                         ],
-                     ],
-                     1 => [
-                         'nested2' => [
-                             0 => ['int' => 4],
-                             1 => ['int' => 5],
-                         ],
-                     ],
-                 ],
-                 'multiple' => [
-                     0 => 'a',
-                     1 => 6665,
-                     2 => 'c',
-                 ],
-                 'integers' => [
-                     123,
-                     1234,
-                     12345,
-                     123456,
-                 ],
-                 'objects' => [
-                     ['id' => 123],
-                     ['id' => 234],
-                     ['id' => 345],
-                 ],
-                 'arrays' => [
-                     [1,2,3],
-                     [3,4,5],
-                 ],
-                 'dateTimes' => [
-                     [
-                         '2019-07-10T12:50:20+00:00',
-                         '2019-02-28T13:50:24+02:00',
-                     ],
-                     [
-                         '02.02.2012 12:12',
-                         '01.01.2011 11:11',
-                     ],
-                 ],
-             ],
+                    [
+                        'id' => 'b84c9b7f-1ebb-49b6-9d18-4305932b2dd1',
+                        'nested1' => [
+                            0 => [
+                                'nested2' => [
+                                    0 => ['int' => 2],
+                                    1 => ['int' => 3],
+                                ],
+                            ],
+                            1 => [
+                                'nested2' => [
+                                    0 => ['int' => 4],
+                                    1 => ['int' => 5],
+                                ],
+                            ],
+                        ],
+                        'multiple' => [
+                            0 => 'a',
+                            1 => 6665,
+                            2 => 'c',
+                        ],
+                        'integers' => [
+                            123,
+                            1234,
+                            12345,
+                            123456,
+                        ],
+                        'objects' => [
+                            ['id' => 123],
+                            ['id' => 234],
+                            ['id' => 345],
+                        ],
+                        'arrays' => [
+                            [1,2,3],
+                            [3,4,5],
+                        ],
+                        'dateTimes' => [
+                            [
+                                '2019-07-10T12:50:20+00:00',
+                                '2019-02-28T13:50:24+02:00',
+                            ],
+                            [
+                                '02.02.2012 12:12',
+                                '01.01.2011 11:11',
+                            ],
+                        ],
+                    ],
             ]
         );
         $this->expectException(InvalidMappingPath::class);
