2cd499432d2050f4fcf1d0b5415d94bfa1388d98
nwinzer <norman.winzer@bitladen.de>
2025-12-05 10:55:32 +0100

    [code] int-or-float wildcard

---
+++ src/Wildcard/IntOrFloatWildcard.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KigaRoo\SnapshotTesting\Wildcard;
+
+use function is_float;
+
+final class IntOrFloatWildcard implements Wildcard
+{
+    /** @var string */
+    private $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function atPath() : string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $mixed
+     */
+    public function match($mixed) : bool
+    {
+        return is_float($mixed) || is_int($mixed);
+    }
+}
